9550c62e2e016e85a0fbe27590e123d40ced9656
shuaibullattil <shuaibullattil7768@gmail.com>
2025-04-24 20:01:48 +0530

    social media icons addes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import React, { useState, useEffect } from "react"
+import Image from "next/image";
 
 export default function Home() {
   const [opacity, setOpacity] = useState(0);
@@ -17,6 +18,48 @@ export default function Home() {
   return(
     <div className="flex justify-center items-center h-screen" style={{backgroundImage:`linear-gradient(to top right, black 40%, red, orange)`}}>
       <div className="grid grid-cols-2 text-white text-center">
+        <div className="flex col-span-2 gap-4 justify-center relative py-4">
+        <a href="https://www.instagram.com/shuaib_11_" target="_blank" rel="noopener noreferrer">
+          <Image
+            src="/insta.webp"
+            alt="Instagram"
+            width={24}
+            height={28}
+            className="rounded-md"
+          />
+        </a>
+
+        <a href="https://www.linkedin.com/in/shuaibullattil" target="_blank" rel="noopener noreferrer">
+          <Image
+            src="/linkedin.webp"
+            alt="LinkedIn"
+            width={24}
+            height={24}
+            className="rounded-md"
+          />
+        </a>
+
+        <a href="https://www.behance.net/shuaibullattil" target="_blank" rel="noopener noreferrer">
+          <Image
+            src="/behance.webp"
+            alt="Behance"
+            width={28}
+            height={28}
+            className="rounded-md"
+          />
+        </a>
+
+        <a href="https://github.com/shuaibullattil" target="_blank" rel="noopener noreferrer">
+          <Image
+            src="/github.webp"
+            alt="GitHub"
+            width={28}
+            height={28}
+            className="rounded-md"
+          />
+        </a>
+        
+        </div>
         <h1 
           className="text-white font-extrabold text-6xl col-span-2 transition-opacity duration-1000"
           style={{ opacity }}
@@ -28,7 +71,7 @@ export default function Home() {
           style={{ opacity, transitionDelay: '300ms' }}
         >
           <p>Full Stack Developer</p>
-          <p>Full Stack Developer</p>
+          <p>UI/UX Designer</p>
         </div>
         <div 
           className="flex col-span-2 justify-center px-8 py-8 transition-opacity duration-1000"
