07f659ddcfb7a1a8f1acc8712282029a94aec5bd
shuaibullattil <shuaibullattil7768@gmail.com>
2025-04-24 17:46:30 +0530

    red text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,15 +4,15 @@ import {Mail} from "lucide-react"
 export default function Home(){
   return(
     <div className="flex justify-center items-center h-screen bg-black">
-      <div className="grid grid-cols-2 text-white">
-        <h1 className="text-orange font-extrabold text-6xl col-span-2">Shuaib Ullattil</h1>
+      <div className="grid grid-cols-2 text-red">
+        <h1 className="text-white font-extrabold text-6xl col-span-2">Shuaib Ullattil</h1>
         <div className="col-span-1 text-center mt-2 font-bold">
           <p>Full Stack Developer</p>
         </div>
         <div className="col-span-1 text-center mt-2 font-bold">
           <p>UI/UX Designer</p>
         </div>
-        <div className="flex col-span-2 gap-2">
+        <div className="flex col-span-2 ga">
         <Mail className="text-white"/><p>shuaibullattil7768@gmail.com</p>
         </div>
       </div>
